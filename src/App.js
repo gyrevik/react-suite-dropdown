4eb1f2b949d166b2c91590f59d4b54a9b591c04c
Alex Smotritsky <alex.smotritsky@gmail.com>
2023-02-08 00:01:21 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Dropdown from "rsuite/Dropdown";
+import "rsuite/dist/rsuite.min.css";
+import CodeIcon from "@rsuite/icons/Code";
+import PageIcon from "@rsuite/icons/Page";
+import DetailIcon from "@rsuite/icons/Detail";
+import FolderFillIcon from "@rsuite/icons/FolderFill";
+import FileDownloadIcon from "@rsuite/icons/FileDownload";
+import FileUploadIcon from "@rsuite/icons/FileUpload";
 
 function App() {
+  const [value, setValue] = useState("New File");
+  const [icon, setIcon] = useState(PageIcon);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Dropdown title={value} icon={<PageIcon />}>
+        <Dropdown.Item icon={<PageIcon />} onSelect={(eventKey, e) => setValue("New File", e)}>
+          New File
+        </Dropdown.Item>
+        <Dropdown.Item icon={<FolderFillIcon />} onSelect={(eventKey, e) => setValue("New File with Current Profile")}>
+          New File with Current Profile
+        </Dropdown.Item>
+        <Dropdown.Item icon={<FileDownloadIcon />} onSelect={(eventKey, e) => setValue("Download As...")}>
+          Download As...
+        </Dropdown.Item>
+        <Dropdown.Item icon={<DetailIcon />} onSelect={(eventKey, e) => setValue("Export PDF")}>
+          Export PDF
+        </Dropdown.Item>
+      </Dropdown>
     </div>
   );
 }
